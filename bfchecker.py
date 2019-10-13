9e979e222537bc4fd8d6601fb60106f79652241e
DrSwad <drswad2013@gmail.com>
2019-10-13 23:41:26 +0600

    Implemented segment trees and debug.h

--- bfchecker.py
+++ bfchecker.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-for i in range(100):
+for i in range(1000):
     # os.system('./generator ' + str(seed) + ' > input.txt')
     os.system('./generator > in')
     # open("in", "w").write(str(i)
     os.system('./bf < in > bfout')
-    os.system('./main < in > out')
+    os.system('./F < in > out')
     if open('bfout').read() != open('out').read():
             sys.stderr.write('Case %d: WA' % (i + 1))
             exit(0)
