1a5f61404e1097c1e27b27fd26f0a945e6000875
DrSwad <drswad2013@gmail.com>
2020-02-03 02:50:50 +0600

    Will try debugging in class

--- Implementations/scc.cpp
+++ Implementations/scc.cpp
@@ -2,50 +2,81 @@
 
 using namespace std;
 
-const int N = 1e5 + 10;
+typedef unsigned int uint;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
 
-int n, m;
-vector<int> adj[N][2];
+class SCC {
+private:
+	int n;
+	viii adj;
 
-bool vis[N];
-stack<int> st;
-int comp[N];
+	vector<bool> vis;
+	stack<int> st;
+	vector<int> comp;
+	bool foundSCC;
 
-void dfs(int at, int source, int mode) {
-	vis[at] = true;
+	void dfs(int at, int source, int mode) {
+		vis[at] = true;
 
-	if (mode == 1) comp[at] = source;
+		if (mode == 1) comp[at] = source;
 
-	for (int to : adj[at][mode])
-		if (!vis[to])
-			dfs(to, source, mode);
+		for (int to : adj[mode][at])
+			if (!vis[to])
+				dfs(to, source, mode);
 
-	if (mode == 0) st.push(at);
-}
+		if (mode == 0) st.push(at);
+	}
+
+public:
+	SCC(int _n, viii _adj) {
+		n = _n;
+		adj = _adj;
+
+		vis.resize(n + 1, false);
+		comp.resize(n + 1, 0);
+
+		foundSCC = false;
+	}
+
+	vector<int> findComponents() {
+		if (foundSCC) return comp;
+
+		for (int i = 1; i <= n; i++)
+			if (!vis[i])
+				dfs(i, 0, 0);
+
+		fill(vis.begin(), vis.end(), false);
+
+		while (!st.empty()) {
+			int source = st.top();
+			st.pop();
+			if (!vis[source])
+				dfs(source, source, 1);
+		}
+
+		foundSCC = true;
+		return comp;
+	}
+};
 
 int main() {
+	int n, m;
 	cin >> n >> m;
+
+	viii adj(2, vii(n + 1));
 
 	while (m--) {
 		int u, v;
 		scanf("%d %d", &u, &v);
 
-		adj[u][0].push_back(v);
-		adj[v][1].push_back(u);
+		adj[0][u].push_back(v);
+		adj[1][v].push_back(u);
 	}
 
-	for (int i = 1; i <= n; i++)
-		if (!vis[i])
-			dfs(i, 0, 0);
-
-	memset(vis, 0, sizeof vis);
-
-	while (!st.empty()) {
-		int source = st.top();
-		st.pop();
-		if (!vis[source])
-			dfs(source, source, 1);
-	}
+	SCC scc = SCC(n, adj);
+	vector<int> comp = scc.findComponents();
 
 	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -12,28 +12,29 @@ typedef pair<int, int> pii;
 #include "debug.h"
 #endif
 
-const int N = 205;
+const int N = int(3e5) + 10;
+const int M = 1505;
 
-int n;
-pii x[N], y[N];
-map<pii, int> slope_cnt;
+int n, m;
+int r1[N], r2[N];
+int c1[N], c2[N];
+vector<int> event_coords;
+vector<pair<int, bool>> events[M];
+unordered_map<int, int> include_carpets[M], exclude_carpets[M];
+int carpet_covers[N];
+map<pii, int> pair_covers;
+int total_covered, max_removal;
 
-int gcd(int a, int b) {
-	if (a < b) swap(a, b);
-	while (b != 0) {
-		int _a = a;
-		a = b;
-		b = _a % b;
+void reset() {
+	event_coords.clear();
+	for (int i = 1; i <= m + 1; i++) {
+		events[i].clear();
+		include_carpets[i].clear();
+		exclude_carpets[i].clear();
 	}
-	return a;
-}
-
-int trapezoids() {
-
-}
-
-int parallelograms() {
-
+	for (int i = 1; i <= n; i++) carpet_covers[i] = 0;
+	pair_covers.clear();
+	total_covered = max_removal = 0;
 }
 
 int main() {
@@ -45,20 +46,90 @@ int main() {
 	int t;
 	cin >> t;
 
-	for (int cs = 1; cs <= t; cs++) {
-		scanf("%d", &n);
-		for (int i = 1; i <= n; i++) scanf("%d %d", &x[i], &y[i]);
-
+	while (t--) {
+		scanf("%d %d", &n, &m);
 		for (int i = 1; i <= n; i++) {
-			for (int j = i + 1; j <= n; j++) {
-				int dx = abs(x[i] - x[j]);
-				int dy = abs(y[i] - y[j]);
-				int dg = gcd(dx, dy);
-				slope_cnt[make_pair(dx / dg, dy / dg)]++;
-			}
+			scanf("%d %d %d %d", &r1[i], &r2[i], &c1[i], &c2[i]);
 		}
 
-		printf("Case %d: %d\n", cs, trapezoids() - 2 * parallelograms);
+		for (int i = 1; i <= n; i++) {
+			event_coords.push_back(c1[i]);
+			events[c1[i]].push_back(make_pair(i, 1));
+
+			event_coords.push_back(c2[i] + 1);
+			events[c2[i] + 1].push_back(make_pair(i, 0));
+		}
+
+		sort(event_coords.begin(), event_coords.end());
+		event_coords.resize(uint(unique(event_coords.begin(), event_coords.end())
+							- event_coords.begin()));
+
+		int prev_coord, at_coord;
+		for (int i = 0; i < (int)event_coords.size(); i++) {
+			at_coord = event_coords[(uint)i];
+
+			if (i > 0) {
+				int jump_len = at_coord - prev_coord;
+				unordered_map<int, int> got_carpets;
+
+				for (int at_row = 1; at_row <= m + 1; at_row++) {
+					if ((int)got_carpets.size() > 0) total_covered += jump_len;
+
+					if ((int)got_carpets.size() == 1) {
+						int carpet_id = (*got_carpets.begin()).first;
+						carpet_covers[carpet_id] += jump_len;
+					}
+					else if ((int)got_carpets.size() == 2) {
+						vector<int> carpet_ids;
+						for (pair<const int, int> &item : got_carpets) carpet_ids.push_back(item.first);
+						pair_covers[make_pair(carpet_ids[0], carpet_ids[1])] += jump_len;
+					}
+
+					for (pair<const int, int> &item : include_carpets[at_row]) got_carpets[item.first] = 1;
+					for (pair<const int, int> &item : exclude_carpets[at_row]) got_carpets.erase(item.first);
+				}
+			}
+
+			for (pii event : events[at_coord]) {
+				int carpet_id = event.x;
+				bool is_starting = event.y;
+
+				if (is_starting) {
+					include_carpets[r1[carpet_id]][carpet_id] = 1;
+					exclude_carpets[r2[carpet_id] + 1][carpet_id] = 1;
+				}
+				else {
+					include_carpets[r1[carpet_id]].erase(carpet_id);
+					exclude_carpets[r2[carpet_id] + 1].erase(carpet_id);
+				}
+			}
+
+			prev_coord = at_coord;
+		}
+
+		int carpet_covers_max1 = 0, carpet_covers_max2 = 0;
+		for (int i = 1; i <= n; i++) {
+			if (carpet_covers[i] >= carpet_covers_max1) {
+				carpet_covers_max2 = carpet_covers_max1;
+				carpet_covers_max1 = carpet_covers[i];
+			}
+			else if (carpet_covers[i] >= carpet_covers_max2) {
+				carpet_covers_max2 = carpet_covers[i];
+			}
+		}
+		max_removal = max(max_removal, carpet_covers_max1 + carpet_covers_max2);
+
+		for (auto item : pair_covers) {
+			int carpet1 = item.x.x;
+			int carpet2 = item.x.y;
+			int covered = item.y + carpet_covers[carpet1] + carpet_covers[carpet2];
+			max_removal = max(max_removal, covered);
+		}
+
+		printf("%d\n", total_covered - max_removal);
+
+		// reset everything
+		reset();
 	}
 
 	return 0;
