779f1e51e3fd9e0432c64912ce8a7d35c0caa97b
DrSwad <drswad2013@gmail.com>
2020-04-05 18:49:10 +0600

    Added Lazy Segment Tree implementation

--- Implementations/hash.cpp
+++ Implementations/hash.cpp
@@ -1,4 +1,4 @@
-typedef unsigned long long hash
+typedef unsigned long long hash;
 #define BASE 79
 const hash MOD = 10000000000000061LL, codes[] = {59, 61, 3, 73, 79, 2, 89, 97, 103, 107, 109, 113, 5, 137, 139, 151, 157, 163, 167, 173, 181, 191, 193, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 293};
 
@@ -25,7 +25,7 @@ void prepocessHash() {
 		pref[i] = current;
 	}
 }
- 
+
 hash substringHash(int i, int j) {
 	if (i == 0) {
 		return pref[j];
